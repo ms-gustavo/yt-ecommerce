eaabdf598e21a80e9c1ea2c1de0c8b26bb9b9686
Gustavo Maia <ms.gustavo@outlook.com>
2024-03-18 10:49:41 -0300

    ProductCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+import { products } from "@/utils/Products";
 import Container from "./components/Container";
 import HomeBanner from "./components/HomeBanner";
+import { truncateText } from "@/utils/TruncateText";
+import ProductCard from "./components/Products/ProductCard";
 
 export default function Home() {
   return (
@@ -7,6 +10,11 @@ export default function Home() {
       <Container>
         <div>
           <HomeBanner />
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {products.map((product: any) => {
+            return <ProductCard key={product.id} data={product} />;
+          })}
         </div>
       </Container>
     </div>
