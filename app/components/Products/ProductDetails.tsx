28660019e7c46d02cd4ebef7aef7ca9a95b0bf52
Gustavo Maia <ms.gustavo@outlook.com>
2024-03-19 09:49:43 -0300

    ProductDetails - changing colors

--- app/components/Products/ProductDetails.tsx
+++ app/components/Products/ProductDetails.tsx
@@ -1,19 +1,60 @@
 "use client";
 
 import { Rating } from "@mui/material";
+import { useCallback, useState } from "react";
+import SetColor from "./SetColor";
 
 interface ProductDetailsProps {
   product: any;
 }
+
+export type CartProductType = {
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  brand: string;
+  selectedImg: SelectedImgType;
+  quantity: number;
+  price: number;
+};
+
+export type SelectedImgType = {
+  color: string;
+  colorCode: string;
+  image: string;
+};
 
 export const Horizontal = () => {
   return <hr className="w-[30%] my-2" />;
 };
 
 const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
+  const [cartProduct, setCartProduct] = useState<CartProductType>({
+    id: product.id,
+    name: product.name,
+    description: product.description,
+    category: product.category,
+    brand: product.brand,
+    selectedImg: { ...product.images[0] },
+    quantity: 1,
+    price: product.price,
+  });
+
+  console.log("cartProduct", cartProduct);
+
   const productRating =
     product.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
     product.reviews.length;
+
+  const handleColorSelect = useCallback(
+    (value: SelectedImgType) => {
+      setCartProduct((prev) => {
+        return { ...prev, selectedImg: value };
+      });
+    },
+    [cartProduct.selectedImg]
+  );
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
@@ -37,11 +78,15 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
         <div>
           <span className="font-semibold">MARCA: {product.brand}</span>
         </div>
-        <div className={product.inStock ? "text-teal-400" : "text-rose-400"}>
+        <div className={product.inStock ? "text-teal-400" : "text-rose-400 "}>
           {product.inStock ? "Em estoque" : "Fora de estoque"}{" "}
         </div>
         <Horizontal />
-        <div>cor</div>
+        <SetColor
+          images={product.images}
+          cartProduct={cartProduct}
+          handleColorSelect={handleColorSelect}
+        />
         <Horizontal />
         <div>quantidade</div>
         <Horizontal />
