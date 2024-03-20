665d81782758bd11783a25fa3e9ceeb273345207
Gustavo Maia <ms.gustavo@outlook.com>
2024-03-19 21:11:40 -0300

    Shopping cart

---
+++ app/cart/CartClient.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useCart } from "@/hooks/useCart";
+import Link from "next/link";
+import { MdArrowBack } from "react-icons/md";
+import Heading from "../components/Heading";
+
+const CartClient = () => {
+  const { cartProducts } = useCart();
+  if (!cartProducts || cartProducts.length === 0) {
+    return (
+      <div className="flex flex-col items-center">
+        <div className="text-2xl">Seu carrinho está vazio</div>
+        <div>
+          <Link
+            href={"/"}
+            className="text-slate-500 flex items-center gap-1 mt-2"
+          >
+            <MdArrowBack />
+            <span>Comece a comprar</span>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Heading title="Carrinho de compras" center />
+      <div className="grid grid-cols-5 text-xs gap-4 pb-2 items-center mt-8">
+        <div className="col-span-2 justify-self-start">PRODUTO</div>
+        <div className="justify-self-center">PREÇO</div>
+        <div className="justify-self-center">QUANTIDADE</div>
+        <div className="justify-self-end">TOTAL</div>
+      </div>
+      <div>
+        {cartProducts &&
+          cartProducts.map((item) => {
+            return <div key={item.id}>{item.name}</div>;
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default CartClient;
