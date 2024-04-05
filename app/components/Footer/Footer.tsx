c3f8601e8147474bd158be2c93773177afb47215
Gustavo Maia <ms.gustavo@outlook.com>
2024-04-05 11:11:02 -0300

    Footer colors fix

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -10,7 +10,7 @@ import {
 
 const Footer = () => {
   return (
-    <footer className="bg-brown text-beige text-sm mt-16">
+    <footer className="bg-slate-700 text-slate-200 text-sm mt-16">
       <Container>
         <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
           <FooterList>
