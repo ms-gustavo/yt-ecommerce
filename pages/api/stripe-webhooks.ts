397e3464f8339973eee8a7660b4eff9b5777b857
Gustavo Maia <ms.gustavo@outlook.com>
2024-04-05 10:29:21 -0300

    Revert "Prisma import fix"
    This reverts commit fcaff404f3d53d9c393d2de3fd6bc7609545a5ff.

--- pages/api/stripe-webhooks.ts
+++ pages/api/stripe-webhooks.ts
@@ -1,7 +1,6 @@
 import { buffer } from "micro";
 import { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
-import prisma from "@/libs/prismadb";
 
 export const config = {
   api: {
